7ea488f324e98dae5abf4100337f9e7d7757473c
yafet-123 <yafetaddisu123@gmail.com>
2022-11-29 03:21:17 -0800

    adding the basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
+      <Head>
+        <title>Roundest Pokemon</title>
+      </Head>
+
+      <div className="text-2xl text-center pt-8 m-2">Which Pokémon is Rounder?</div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-out",
+      },
+    },
   },
   plugins: [],
 }
